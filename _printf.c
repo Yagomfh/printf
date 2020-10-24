de7dbcc7f73eaea150699183d86b133bd3558390
Gregory ARTHUS <garthus@adminux.fr>
2020-10-24 09:18:39 +0000

    basic printf without %

--- _printf.c
+++ _printf.c
@@ -3,13 +3,21 @@
 
 int _printf(const char *format, ...)
 {
-	type_d 
-	int i;
+	type_d cases[] = {{"c", print_char},{"s", print_string}};
+	unsigned int i, j;
 	va_list values;
+
+	va_start(values, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (
-		if (format[i] == '%'
-
+		if (format[i - 1] == '%' && format[i - 2] != '\\')
+		{
+			for (j = 0; cases[j].type != NULL; j++)
+			{
+				cases[j].f(values);
+			}
+		}
+		_putchar(format[i]);
+	 }
 }
