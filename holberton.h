096ebb71427f52c1692d60a59962bc5401e67d50
yagomfh <yagomfh@gmail.com>
2020-10-24 09:09:34 +0000

    feat(holberton.h) : adding new protos

--- holberton.h
+++ holberton.h
@@ -1,9 +1,22 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+/**
+ * struct form - type struct
+ * @type: letter type
+ * @f: fuction to call
+ *
+ * Description: fuction to call depending on char call
+ */
+
 typedef struct form
 {
 	char *type;
-	(*f)(va_list a);
+	void (*f)(va_list a);
 }
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+void print_char(va_list a);
+void print_string(va_list a);
 #endif
