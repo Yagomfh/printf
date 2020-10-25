0805bac10fa97314ade23d231fbf73f3fff77ae9
Gregory ARTHUS <garthus@adminux.fr>
2020-10-25 18:58:10 +0000

    print Non printable characters betty style

--- print_string_npc.c
+++ print_string_npc.c
@@ -10,26 +10,26 @@ int print_string_npc(va_list a)
 {
 	char *tmp = va_arg(a, char*);
 	char s;
-	int j; 
+	int j;
 
 	j = 0;
 	while (tmp[j])
 	{
 		if (tmp[j] < 32 || tmp[j] >= 127)
 		{
-				s = tmp[j];
-				if(s < 10)
-				{
-					s = s + 48;
-				}
-				else
-				{
-					s = s + 55;
-				}
-				_putchar('\\');
-				_putchar('x');
-				_putchar('0');
-				_putchar(s);
+			s = tmp[j];
+			if (s < 10)
+			{
+				s = s + 48;
+			}
+			else
+			{
+				s = s + 55;
+			}
+			_putchar('\\');
+			_putchar('x');
+			_putchar('0');
+			_putchar(s);
 		}
 		else
 		{
