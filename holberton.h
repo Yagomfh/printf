46b92bc64881f3a922c98a9cbf2269b1f602257e
Gregory ARTHUS <garthus@adminux.fr>
2020-10-25 09:32:12 +0000

    unsigned int

--- holberton.h
+++ holberton.h
@@ -24,4 +24,5 @@ int print_char(va_list a);
 int print_string(va_list a);
 int print_int(va_list a);
 int print_bin(va_list a);
+int print_unsigned_int(va_list a);
 #endif
--- main.c
+++ main.c
@@ -11,23 +11,24 @@ int main(void)
 {
 	int len;
 	int len2;
-/**	unsigned int ui;
-	void *addr;
+	unsigned int ui;
+/*	void *addr;
 */
 	len = 120000;
 	len2 = 4;
+	ui = -12;
 
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
 /**	ui = (unsigned int)INT_MAX + 1024;**/
 	_printf("Length:[%d, %i]\n", len, len2);
 	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -2147483648);
-	_printf("%b\n", 98);
-	/**	printf("Negative:[%d]\n", -2147483648);
+	_printf("Negative:[%d]\n", -2147);
+/**	_printf("%b\n", 98); **/
+	printf("Negative:[%d]\n", -2648);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
+/**	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui); **/
