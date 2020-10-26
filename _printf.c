70725de2c6fc784c553f08e42422f64b1dcfa9da
yagomfh <yagomfh@gmail.com>
2020-10-26 17:06:03 +0000

    fix(_printf.c) : null condition

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 		return (0);
 	va_start(values, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
