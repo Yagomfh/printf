0b06b44e728dc5a71d8d47e1de09fe9b8216c2e9
Gregory ARTHUS <garthus@adminux.fr>
2020-10-26 01:45:25 +0000

    debug():change algo

--- print_string_npc.c
+++ print_string_npc.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
 
 /**
+ * equal127 - char equal to del 127
+ * @s: a char
+ * Return: none
+ **/
+
+void equal127(char s)
+{
+	_putchar('\\');
+	_putchar('x');
+	_putchar('7');
+	_putchar('F');
+}
+
+/**
  * print_string_npc - print string non printable char
  * @a: a string
  * Return: number of char
@@ -10,32 +24,41 @@ int print_string_npc(va_list a)
 {
 	char *tmp = va_arg(a, char*);
 	char s;
-	int j;
+	int j = 0, len = 0;
 
-	j = 0;
 	while (tmp[j])
 	{
-		if (tmp[j] < 32 || tmp[j] >= 127)
+		if (tmp[j] < 32)
 		{
 			s = tmp[j];
+			_putchar('\\');
 			if (s < 10)
 			{
 				s = s + 48;
+				_putchar('x');
+				_putchar('0');
 			}
 			else
 			{
 				s = s + 55;
+				_putchar('x');
+				_putchar('1');
 			}
-			_putchar('\\');
-			_putchar('x');
-			_putchar('0');
 			_putchar(s);
+			len += 4;
+		}
+		else if (tmp[j] == 127)
+		{
+			s = tmp[j];
+			equal127(s);
+			len += 4;
 		}
 		else
 		{
 			_putchar(tmp[j]);
+			len++;
 		}
 		j++;
 	}
-	return (j);
+	return (len);
 }
