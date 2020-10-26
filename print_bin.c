c5d04838ee9e335e798780c8969635516adf23fa
yagomfh <yagomfh@gmail.com>
2020-10-26 18:23:55 +0000

    undo changes

--- print_bin.c
+++ print_bin.c
@@ -9,28 +9,19 @@
 
 int print_bin(va_list a, char __attribute__((unused))flag_c)
 {
-	int tmp = va_arg(a, int);
+	unsigned int tmp = va_arg(a, int);
 	int binTable[33];
 	int i, j, len = 0;
 
 	i = 0;
-	if (tmp < 0)
+	while (tmp > 0)
 	{
-		_putchar('%');
-		_putchar('b');
-		return (32);
+		binTable[i] = tmp % 2;
+		tmp = (tmp - tmp % 2) / 2;
+		i++;
+		len++;
 	}
-	else
-	{
-		while (tmp > 0)
-		{
-			binTable[i] = tmp % 2;
-			tmp = (tmp - tmp % 2) / 2;
-			i++;
-			len++;
-		}
-		for (j = i - 1; j >= 0; j--)
-			_putchar(binTable[j] + '0');
-	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(binTable[j] + '0');
 	return (len);
 }
