05c303ec2449008511607b6ec10278f5bff50477
yagomfh <yagomfh@gmail.com>
2020-10-26 20:28:02 +0000

    fix(print_rot13.c) : betty complaint

--- print_rot13.c
+++ print_rot13.c
@@ -1,4 +1,22 @@
 #include "holberton.h"
+/**
+* _strlen - the length of a string
+* @s: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int result = 0;
+
+	while (*s != '\0')
+	{
+		result++;
+		s++;
+	}
+	return (result);
+}
 
 /**
  * print_rot13 - print string in rot13
@@ -11,12 +29,12 @@ int print_rot13(va_list a, char __attribute__((unused))flag_c)
 {
 	char *s = va_arg(a, char*);
 	char *tmp;
-	int i, j;
+	int i, j, len;
 	char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char *rotate = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; s[i] != '\0'; i++)
-			tmp = malloc(sizeof(char) * i);
+	len = _strlen(s);
+	tmp = malloc(sizeof(char) * len);
 	if (tmp == NULL)
 		return (0);
 	for (i = 0; s[i] != '\0'; i++)
